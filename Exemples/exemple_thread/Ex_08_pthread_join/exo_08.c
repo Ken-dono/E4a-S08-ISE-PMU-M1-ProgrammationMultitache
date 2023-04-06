8fea95f7ebac9d2521044cbc77b116f4c3363f8e
Ken-dono <kendono0206@gmail.com>
2023-04-06 08:58:53 +0200

    exo_08.c

--- Exemples/exemple_thread/Ex_08_pthread_join/exo_08.c
+++ Exemples/exemple_thread/Ex_08_pthread_join/exo_08.c
@@ -6,19 +6,19 @@
 void *thread_lecture_fct();
 void *thread_affichage_fct();
 char lettre_a_trouver;
+char lettre_condition_sortie = 'q';
 
 int main(void)
 {
     pthread_t thread_lecture;
     pthread_t thread_affichage;
 
-
     if (pthread_create(&thread_lecture, NULL, thread_lecture_fct, &lettre_a_trouver) != 0) {
         fprintf(stderr, "Erreur pthread_create pour le thread lecture\n");
         exit(EXIT_FAILURE);
     }
 
-    if (pthread_create(&thread_affichage, NULL, thread_affichage_fct, NULL) != 0) {
+    if (pthread_create(&thread_affichage, NULL, thread_affichage_fct, &lettre_a_trouver) != 0) {
         fprintf(stderr, "Erreur pthread_create pour le thread affichage\n");
         exit(EXIT_FAILURE);
     }
@@ -33,16 +33,23 @@ int main(void)
 
 void *thread_lecture_fct()
 {
-    scanf(" %c", &lettre_a_trouver);
-    printf("Lettre lue : %c , arrêt du thread de lecture\n", lettre_a_trouver);
+    while (lettre_a_trouver != lettre_condition_sortie) {
+        scanf(" %c", &lettre_a_trouver);
+        printf("Lettre lue : %c\n", lettre_a_trouver);
+    }
+    printf("Arrêt du thread de lecture\n");
     return NULL;
 }
 
 void *thread_affichage_fct()
 {
-    while (lettre_a_trouver == 0) {
+    while (lettre_a_trouver != lettre_condition_sortie) {
+        if (lettre_a_trouver != 0) {
+            printf("Lettre affichée : %c\n", lettre_a_trouver);
+            lettre_a_trouver = 0;
+        }
         sleep(1);
     }
-    printf("Lettre affichée : %c , arrêt du thread d'affichage\n", lettre_a_trouver);
+    printf("Arrêt du thread d'affichage\n");
     return NULL;
 }
