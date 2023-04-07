838b637ead3c522c9982aa2d07a939304499c5c9
Ken-dono <kendono0206@gmail.com>
2023-04-07 11:24:21 +0200

    exo_17

--- Exemples/Exemple_3/Ex_17_fork_exec_wait_signal/main.c
+++ Exemples/Exemple_3/Ex_17_fork_exec_wait_signal/main.c
@@ -13,68 +13,66 @@
 
 static int run = 1;
 
-void signal_sigint_handler(int signal)
+void signal_sigint_handler(int)
 {
-	fprintf(stdout, "\nSIGINT intercepté (PID %d)\n", getpid());
-	run = 0;
+    fprintf(stdout, "\nSIGINT intercepté (PID %d)\n", getpid());
+    run = 0;
 }
 
-void signal_sigchld_handler(int signal)
+void signal_sigchld_handler(int)
 {
-	fprintf(stdout, "SIGCHLD intercepté (PID %d)\n", getpid());
-	wait(NULL);
+    fprintf(stdout, "SIGCHLD intercepté (PID %d)\n", getpid());
+    wait(NULL);
 }
 
 int main(void)
 {
-	/* interception du Ctrl+C pour sortie propre */
-	signal(SIGINT, signal_sigint_handler);
+    /* interception du Ctrl+C pour sortie propre */
+    signal(SIGINT, signal_sigint_handler);
 
-	/* interception de SIGCHLD pour suppression de zombis à tout moment */
-	signal(SIGCHLD, signal_sigchld_handler);
+    /* interception de SIGCHLD pour suppression de zombis à tout moment */
+    signal(SIGCHLD, signal_sigchld_handler);
 
-	pid_t pid_fils;
+    pid_t pid_fils;
 
-	pid_fils = fork();
+    pid_fils = fork();
 
-	if (pid_fils == -1) {
-		fprintf(stderr, "fork() impossible, errno = %d\n", errno);
-		exit(EXIT_FAILURE);
-	}
+    if (pid_fils == -1) {
+        fprintf(stderr, "fork() impossible, errno = %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
 
-	if (pid_fils == 0) {
-		/* processus fils */
-		fprintf(stdout, "Fils : PID = %ld, PPID = %ld\n", (long)getpid(), (long)getppid());
+    if (pid_fils == 0) {
+        /* processus fils */
+        fprintf(stdout, "Fils : PID = %ld, PPID = %ld\n", (long)getpid(), (long)getppid());
 
-		/* lancement d'un programme dans le processus fils */
-		char * argv[3];
+        /* lancement d'un programme dans le processus fils */
+        char * argv[3];
 
-		argv[0] = "xclock";
-		argv[1] = "-digital";
-		argv[2] = (char *) NULL;
+        argv[0] = "xclock";
+        argv[1] = "-digital";
+        argv[2] = (char *) NULL;
 
-		fprintf(stdout, "Fils : lancement de %s %s\n", argv[0], argv[1]);
-		fflush(stdout);
+        fprintf(stdout, "Fils : lancement de %s %s\n", argv[0], argv[1]);
+        fflush(stdout);
 
-		execv("/usr/bin/xclock", argv);
+        execv("/usr/bin/xclock", argv);
 
-		fprintf(stderr, "erreur exec\n");
-		fflush(stderr);
-		exit(EXIT_FAILURE);
+        fprintf(stderr, "erreur exec\n");
+        fflush(stderr);
+        exit(EXIT_FAILURE);
 
-	} else {
-		/* processus père */
-		fprintf(stdout, "Père : PID = %ld, PPID = %ld, PID fils = %ld\n", (long)getpid(), (long)getppid(), (long)pid_fils);
-		
-		/* exécution du traitement */
-		while (run) {
-			fprintf(stdout, "Père : exec.\n");
-			sleep(1);
-		}
+    } else {
+        /* processus père */
+        fprintf(stdout, "Père : PID = %ld, PPID = %ld, PID fils = %ld\n", (long)getpid(), (long)getppid(), (long)pid_fils);
 
-		fprintf(stdout, "Père : exit\n");
-		exit(EXIT_SUCCESS);
-	}
+        /* exécution du traitement */
+        while (run) {
+            fprintf(stdout, "Père : exec.\n");
+            sleep(1);
+        }
+
+        fprintf(stdout, "Père : exit\n");
+        exit(EXIT_SUCCESS);
+    }
 }
-
-
